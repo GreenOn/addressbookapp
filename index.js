28aea1cc20b4cde2e7f5ee5dee8267c771e44683
Pramod Vadrevu <pramodvadrevu@gmail.com>
2017-05-06 16:29:56 -0500

    Corrected the route for staticServe.

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ var server = restify.createServer();
 
 const port = 8080;
 
+const dirname = './client';
 
+
+server.get('/about', restify.serveStatic({
+  directory: './client',
+  file: 'about.html'
+}));
+
+server.get(/\/?.*/, restify.serveStatic({
+  directory: './client',
+  default:'index.html'
+}));
 
 server.listen(port, function(){
 	console.log("%s listeneing on %s ", server.name, port);
